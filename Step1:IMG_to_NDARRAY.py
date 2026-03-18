10ff9a4978f4e593a7f9fd485c432406d5c474c6
The System Programmer <the.system.programmer.2004@gmail.com>
2026-03-18 09:42:00 +0530

    Using trymesh to make mesh

--- Step1:IMG_to_NDARRAY.py
+++ Step1:IMG_to_NDARRAY.py
@@ -6,7 +6,7 @@ rows = 2880
 cols = 5760
 
 # Load raster
-p = Path("/home/linux/Code/Mars-Topography-Project/megt90n000eb.img")
+p = Path("/home/linux/Code/Mars-Topography-Project/data/megt90n000eb.img")
 data = np.fromfile(p,dtype=">i2",count = rows*cols).reshape((rows,cols)).astype(np.float32)
 data[data <= -32768] = np.nan
 
@@ -14,4 +14,4 @@ data[data <= -32768] = np.nan
 data = np.flipud(data)
 
 # Export data as a .npy file
-np.save("mola_elevation.npy",data)
+np.save("data/mola_elevation.npy",data)
---
+++ Step2:Build_Mesh.py
@@ -0,0 +1,3 @@
+import numpy as np
+import trimesh
+data = np.load("data/mola_elevation.npy")
